e46e46bb871a54d55748afe9303d348bfcaae45d
Joshua Potter <tljoshh@gmail.com>
2018-09-08 00:08:48 -0400

    updated AccountTest to match fig 3.9

--- Assignment 1/AccountTest.java
+++ Assignment 1/AccountTest.java
@@ -11,32 +11,29 @@ public class AccountTest {
         // display initial balance of each object
         displayAccount(account1);
         displayAccount(account2);
-        System.out.println();
 
         // create a Scanner to obtain input from the command window
         Scanner input = new Scanner(System.in);
 
         System.out.print("Enter deposit amount for account1: "); // prompt
         double depositAmount = input.nextDouble(); // obtain user input
-        System.out.printf("%nadding %.2f to account1 balance%n%n",
+        System.out.printf("adding %.2f to account1 balance%n",
             depositAmount);
         account1.deposit(depositAmount); // add to account1’s balance
 
         // display balances
         displayAccount(account1);
         displayAccount(account2);
-        System.out.println();
 
         System.out.print("Enter deposit amount for account2: "); // prompt
         depositAmount = input.nextDouble(); // obtain user input
-        System.out.printf("%nadding %.2f to account2 balance%n%n",
+        System.out.printf("adding %.2f to account2 balance%n",
             depositAmount);
         account2.deposit(depositAmount); // add to account2 balance
 
         // display balances
         displayAccount(account1);
         displayAccount(account2);
-        System.out.println();
 
         input.close();
     }
