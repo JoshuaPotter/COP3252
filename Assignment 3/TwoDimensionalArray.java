8c4bdf3e0c1697b8259b2da69fc07a8ca0189f1d
Joshua Potter <tljoshh@gmail.com>
2018-09-25 02:17:49 -0400

    2d array can scale

--- Assignment 3/TwoDimensionalArray.java
+++ Assignment 3/TwoDimensionalArray.java
@@ -6,31 +6,28 @@ import java.security.SecureRandom; // random numbers
 public class TwoDimensionalArray {
     private static final SecureRandom randomNumbers = new SecureRandom();
     public static void main(String[] args) {
-        int[][] array = new int[4][6]; // 4 col, 6 row
-
-        // System.out.printf("Columns: %d%n", array.length); // col
-        // System.out.printf("Rows: %d%n%n", array[0].length); // row
+        int[][] array = new int[4][6]; // 4 col, 6 row (works at any size)
         
         // fill with random numbers
         for(int col = 0; col < array.length; col++) {
-            if(col < 3) {
+            if(col < array.length-1) {
                 for(int row = 0; row < array[0].length; row++) {
-                    if(row < 5) {
+                    if(row < array[0].length-1) {
                         // add random number (0-9) to cell
                         array[col][row] = randomNumbers.nextInt(10);
                     }
                 }
             } else {
-                array[3][5] = 0;
+                array[array.length-1][array[0].length-1] = 0;
             }
         }
 
         // sum columns
         for(int col = 0; col < array.length; col++) {
-            if(col < 3) {
+            if(col < array.length-1) {
                 for(int row = 0; row < array[0].length; row++) {
-                    if(row < 5) {
-                        array[col][5] += array[col][row]; 
+                    if(row < array[0].length-1) {
+                        array[col][array[0].length-1] += array[col][row]; 
                     }
                 }
             }
@@ -38,28 +35,22 @@ public class TwoDimensionalArray {
 
         // sum rows
         for(int row = 0; row < array[0].length; row++) {
-            if(row < 5) {
+            if(row < array[0].length-1) {
                 for(int col = 0; col < array.length; col++) {
-                    if(col < 3) {
-                        array[3][row] += array[col][row];
+                    if(col < array.length-1) {
+                        array[array.length-1][row] += array[col][row];
                     }
                 }
             }
         }
 
         // print table
-        System.out.printf("(Row, Column): Integer%n");
         for(int row = 0; row < array[0].length; row++) {
-            System.out.println();
             for(int col = 0; col < array.length; col++) {
-                if(col == 3 && row == 5) {
-                    // (6, 4): 0
-                } else if (col == 3) {
-                    System.out.print("Sum of row: ");
-                } else if (row == 5) {
-                    System.out.print("Sum of column: ");
+                System.out.printf("%-2d ", array[col][row]);
+                if(col == array.length-1) {
+                    System.out.println();
                 }
-                System.out.printf("(%d, %d): %d%n", row+1, col+1, array[col][row]);
             }
         }
     }
