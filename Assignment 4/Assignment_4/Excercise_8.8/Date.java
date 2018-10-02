487fa047c91cad0f231f4490dbe5f837194c6e2d
Joshua Potter <tljoshh@gmail.com>
2018-10-02 01:27:11 -0400

    Added Date class

---
+++ Assignment 4/Assignment_4/Excercise_8.8/Date.java
@@ -0,0 +1,62 @@
+// Fig. 8.7: Date.java 
+// Date class declaration.
+
+public class Date {
+    private int month; // 1-12
+    private int day; // 1-31 based on month
+    private int year; // any year
+
+    private static final int[] daysPerMonth = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+    // constructor: confirm proper value for month and day given the year
+    public Date(int month, int day, int year) {
+        // check if month in range
+        if (month <= 0 || month > 12) {
+            throw new IllegalArgumentException("month (" + month + ") must be 1-12");
+        }
+
+        // check if day in range for month
+        if (day <= 0 || (day > daysPerMonth[month] && !(month == 2 && day == 29))) {
+            throw new IllegalArgumentException("day (" + day + ") out-of-range for the specified month and year");
+        }
+
+        // check if year is in range
+        if (year <= 0) {
+            throw new IllegalArgumentException("year (" + year + ") out-of-range for the specified year");
+        }
+
+        // check for leap year if month is 2 and day is 29
+        if (month == 2 && day == 29 && !(year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))) {
+            throw new IllegalArgumentException("day (" + day + ") out-of-range for the specified month and year");
+        }
+
+        this.month = month;
+        this.day = day;
+        this.year = year;
+
+        System.out.printf("Date object constructor for date %s%n", this);
+    }
+
+    // increment member variables to next day
+    public void nextDay() {
+        // check if incremented day is still in month, 
+        //  and increment month or year if necessary
+        if(this.day == daysPerMonth[this.month]) {
+            this.day = 1;
+            
+            if(this.month == 12) {
+                this.month = 1;
+                this.year++;
+            } else {
+                this.month++;
+            }
+        } else {
+            this.day++;
+        }
+    }
+
+    // return a String of the form month/day/year
+    public String toString() {
+        return String.format("%d/%d/%d", month, day, year);
+    }
+}
---
+++ Assignment 4/Assignment_4/Excercise_8.8/DateTest.java
@@ -0,0 +1,12 @@
+// Fig. 8.8: DateTest.java
+
+public class DateTest {
+    public static void main(String[] args) {
+        Date d1 = new Date(12,1,1990); // month, day, year 
+
+        for(int i = 0; i < 90; i++) {
+            d1.nextDay();
+            System.out.printf("+%d day(s): %s%n", i+1, d1.toString());
+        }
+    }
+}
