89e3e5449795d356936f13e50e0046d63cadd040
Joshua Potter <tljoshh@gmail.com>
2018-10-02 00:43:11 -0400

    Adding Excercise 8.7

--- Assignment 4/Assignment_4/Excercise_8.7/Time2.java
+++ Assignment 4/Assignment_4/Excercise_8.7/Time2.java
@@ -1,37 +1,25 @@
-// Joshua Potter, jop13
 // Fig. 8.5: Time2.java
 // Time2 class declaration with overloaded constructors.  
 
 public class Time2 {
-   private int secondsSinceMidnight; // 0 - 23
+   private int hour; // 0 - 23
+   private int minute; // 0 - 59
+   private int second; // 0 - 59
 
    // Time2 no-argument constructor:
    // initializes each instance variable to zero
    public Time2() {
-      this.secondsSinceMidnight = 0; // invoke constructor with three arguments
+      this(0, 0, 0); // invoke constructor with three arguments
    }
 
    // Time2 constructor: hour supplied, minute and second defaulted to 0
    public Time2(int hour) {
-      if (hour < 0 || hour >= 24) {
-         throw new IllegalArgumentException("hour must be 0-23");
-      }
-
-      this.secondsSinceMidnight = (hour * 60) * 60; // invoke constructor with three arguments
+      this(hour, 0, 0); // invoke constructor with three arguments
    }
 
    // Time2 constructor: hour and minute supplied, second defaulted to 0
    public Time2(int hour, int minute) {
-      if (hour < 0 || hour >= 24) {
-         throw new IllegalArgumentException("hour must be 0-23");
-      }
-
-      if (minute < 0 || minute >= 60) {
-         throw new IllegalArgumentException("minute must be 0-59");
-      }
-
-      // convert hour and minutes to seconds and add
-      this.secondsSinceMidnight = (hour * 60) * 60 + minute * 60; // invoke constructor with three arguments
+      this(hour, minute, 0); // invoke constructor with three arguments
    }
 
    // Time2 constructor: hour, minute and second supplied
@@ -48,14 +36,15 @@ public class Time2 {
          throw new IllegalArgumentException("second must be 0-59");
       }
 
-      // convert hour and minutes to seconds and add with seconds parameter
-      this.secondsSinceMidnight = (hour * 60) * 60 + minute * 60 + second;
+      this.hour = hour;
+      this.minute = minute;
+      this.second = second;
    }
 
    // Time2 constructor: another Time2 object supplied
    public Time2(Time2 time) {
       // invoke constructor with three arguments
-      this.secondsSinceMidnight = time.secondsSinceMidnight;
+      this(time.hour, time.minute, time.second);
    }
 
    // Set Methods
@@ -74,8 +63,9 @@ public class Time2 {
          throw new IllegalArgumentException("second must be 0-59");
       }
 
-      // convert hour and minutes to seconds and add with seconds parameter
-      this.secondsSinceMidnight = (hour * 60) * 60 + minute * 60 + second;
+      this.hour = hour;
+      this.minute = minute;
+      this.second = second;
    }
 
    // validate and set hour
@@ -84,7 +74,7 @@ public class Time2 {
          throw new IllegalArgumentException("hour must be 0-23");
       }
 
-      this.secondsSinceMidnight = (hour * 60) * 60;
+      this.hour = hour;
    }
 
    // validate and set minute
@@ -93,7 +83,7 @@ public class Time2 {
          throw new IllegalArgumentException("minute must be 0-59");
       }
 
-      this.secondsSinceMidnight = minute * 60;
+      this.minute = minute;
    }
 
    // validate and set second
@@ -102,23 +92,23 @@ public class Time2 {
          throw new IllegalArgumentException("second must be 0-59");
       }
 
-      this.secondsSinceMidnight = second;
+      this.second = second;
    }
 
    // Get Methods
    // get hour value
    public int getHour() {
-      return (this.secondsSinceMidnight % 86400) / 3600;
+      return hour;
    }
 
    // get minute value
    public int getMinute() {
-      return ((this.secondsSinceMidnight % 86400) % 3600) / 60;
+      return minute;
    }
 
    // get second value
    public int getSecond() {
-      return ((this.secondsSinceMidnight % 86400) % 3600) % 60;
+      return second;
    }
 
    // convert to String in universal-time format (HH:MM:SS)
@@ -131,4 +121,59 @@ public class Time2 {
       return String.format("%d:%02d:%02d %s", ((getHour() == 0 || getHour() == 12) ? 12 : getHour() % 12), getMinute(),
             getSecond(), (getHour() < 12 ? "AM" : "PM"));
    }
+
+   // increment seconds
+   public void tick() {
+      // ensure seconds and minutes dont exceed 59
+      //    and increment other values correctly if
+      //    there is overflow
+      if(this.second == 59) {
+         this.second = 0;
+
+         if(this.minute == 59) {
+            this.minute = 0;
+
+            if(this.hour == 23) {
+               this.hour = 0;
+               this.minute = 0;
+               this.second = 0;
+            } else {
+               this.hour++;
+            }
+         } else {
+            this.minute++;
+         }
+      } else {
+         this.second++;
+      }
+   }
+
+   // increment minutes
+   public void incrementMinute() {
+      // increment 60 seconds ahead
+      for(int i = 0; i < 60; i++) {
+         tick();
+      }
+   }
+
+   // increment minutes
+   public void incrementHour() {
+      // increment 3600 seconds ahead
+      for(int i = 0; i < 3600; i++) {
+         tick();
+      }
+   }
 }
+
+/**************************************************************************
+ * (C) Copyright 1992-2018 by Deitel & Associates, Inc. and * Pearson Education,
+ * Inc. All Rights Reserved. * * DISCLAIMER: The authors and publisher of this
+ * book have used their * best efforts in preparing the book. These efforts
+ * include the * development, research, and testing of the theories and programs
+ * * to determine their effectiveness. The authors and publisher make * no
+ * warranty of any kind, expressed or implied, with regard to these * programs
+ * or to the documentation contained in these books. The authors * and publisher
+ * shall not be liable in any event for incidental or * consequential damages in
+ * connection with, or arising out of, the * furnishing, performance, or use of
+ * these programs. *
+ *************************************************************************/
